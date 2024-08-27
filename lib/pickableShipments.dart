5a38bee2976444f521fea8e2fd5d23af88710dbf
khalidfahmy98 <khaled741@beone.sa>
2024-08-27 17:23:10 +0300

    ListView inserted in pickable shipments list

--- lib/pickableShipments.dart
+++ lib/pickableShipments.dart
@@ -2,6 +2,7 @@ import 'package:beonecouriers/includes/barCard.dart';
 import 'package:beonecouriers/includes/menuTabs.dart';
 import 'package:beonecouriers/includes/pageLabel.dart';
 import 'package:flutter/material.dart';
+import 'data/dataExample.dart';
 
 class PickableShipments extends StatefulWidget {
   const PickableShipments({super.key});
@@ -17,16 +18,26 @@ class _PickableShipmentstate extends State<PickableShipments> {
       body: Column(
         children: [
            const BarCard(),
-           Expanded(
-            child: Column(
-              children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  child : const PageLabel(subject: 'My Pickable Shipments'),
-                ),
-              ],
-            ),
-          ),
+           Column(
+             children: [
+               Container(
+                 alignment: Alignment.centerLeft,
+                 child : const PageLabel(subject: 'My Pickable Shipments'),
+               ),
+               Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                    itemBuilder: (context , index){
+                      return Card(
+                        child: Column(
+
+                        ),
+                      );
+                    }
+                  ),
+               ),
+             ],
+           ),
           const MenuTabs(selectedIndex: 0),
         ],
       ),
