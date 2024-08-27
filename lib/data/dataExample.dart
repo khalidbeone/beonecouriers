c6932af26cb0a80278e35a19c79714089e4cbbd5
khalidfahmy98 <khaled741@beone.sa>
2024-08-27 14:28:14 +0300

    created examples data list

---
+++ lib/data/dataExample.dart
@@ -0,0 +1,45 @@
+
+
+
+final List<dynamic> shipmentsExample = 
+[
+    {
+      "id" : 256 , 
+      "awb_no" : "BEO8442944533",
+      "sender_name" : "ZidShip",
+      "sender_phone" : "0507310296",
+      "sender_city" : "Riyadh",
+      "reciver_name" : "محمد بن علي السلطان" , 
+      "receiver_address" : "Riyadh , Al rabwa st 2556",
+      "receiver_city" : "Riyadh",
+      "reciever_phone" : "0507310296",
+      "cod_amount": 0,
+      "shipment_status" : 26 
+    },
+     {
+      "id" : 257 , 
+      "awb_no" : "BEO8442784533",
+      "sender_name" : "ZidShip",
+      "sender_phone" : "0507310296",
+      "sender_city" : "Riyadh",
+      "reciver_name" : "Moahmmed Mamdouh" , 
+      "receiver_address" : "Riyadh , Al rabwa st 2556",
+      "receiver_city" : "Unyezah",
+      "reciever_phone" : "0507310296",
+      "cod_amount": 0,
+      "shipment_status" : 26 
+    },
+     {
+      "id" : 256 , 
+      "awb_no" : "BEO214944533",
+      "sender_name" : "ZidShip",
+      "sender_phone" : "0507310296",
+      "sender_city" : "Jeddah",
+      "reciver_name" : "Khalid Fahmy" , 
+      "receiver_address" : "Riyadh , Al rabwa st 2556",
+      "receiver_city" : "Dammam",
+      "reciever_phone" : "0507310296",
+      "cod_amount": 0,
+      "shipment_status" : 26 
+    },
+];
