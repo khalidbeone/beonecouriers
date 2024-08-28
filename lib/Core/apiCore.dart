5fa668aa39ac940c800c8f5352a4bcbbbd7001ad
khalidfahmy98 <khaled741@beone.sa>
2024-08-28 16:42:19 +0300

    cleaned

---
+++ lib/Core/apiCore.dart
@@ -0,0 +1,2 @@
+const  String demoBaseUrl = 'https://demo.beone.com.sa/api/courier/v1/';
+const List<String> methods = ['login','refresh','logout' , 'profile'];
--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -1,10 +1,5 @@
-import 'package:beonecouriers/homepage.dart';
 import 'package:beonecouriers/includes/copyRights.dart';
 import 'package:flutter/material.dart';
-import 'Box/sessionBox.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
 
 
 class LoginPage extends StatefulWidget {
@@ -19,58 +14,7 @@ class _LoginPageState extends State<LoginPage> {
   final   TextEditingController _email = TextEditingController() ;
   final   TextEditingController _password = TextEditingController() ;
 
-   List<Map<String,dynamic>> courierInfo  = [];
-   List<Map<String,dynamic>> authInfo  = [];
-    
-    Future<Map<String, dynamic>>  authingFunction (email , password) async{
-        try{
-          final res = await http.post(
-            Uri.parse(demoBaseUrl + methods[0]),
-            headers: {"Content-Type": "application/json" , "Accept" : "application/json"} ,
-            body:jsonEncode({ "email" : email , "password" : password})
-          );
-          final data = jsonDecode(res.body);
-          if( data['status'] == 200 ){
-            final courierData  = await http.get(Uri.parse(demoBaseUrl + methods[3]),
-              headers: {"Content-Type": "application/json" , "Accept" : "application/json" , 'Authorization': data['data']['access_token'] },
-            );
-            final courier  = jsonDecode(courierData.body);
-            if( courier['status'] == 200 ){
-              setState(() {
-                authInfo = data ;
-                courierInfo = courier ;
-              });
-            }
-          }
-          return  data;
-        }catch(e){
-          throw  e.toString();
-        }
-      }
-
-
-  void loginCourier(){
-    if( _email.text.toString() != '' || _password.text.toString() != ''){
-        // authingFunction(_email.text , _password.text);
-             print(authInfo['data']['access_token']);
-              print(authInfo['data']['token_type']);
-              print(courierInfo['data']['id']);
-              print(courierInfo['data']['name']);
-
-
-        if(box.read('email') != ''  && box.read('token') != '' ){
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Homepage()));
-        }else{
-          ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content:  Text('Invalid Information | Contact Support ' , style: TextStyle(fontWeight: FontWeight.bold),) , backgroundColor: Colors.red,)
-              );    
-          }
-    }else{
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content:  Text('Invalid Information | Contact Support ' , style: TextStyle(fontWeight: FontWeight.bold),) , backgroundColor: Colors.red,)
-      );
-    }
-  }
+   
 
   @override
   Widget build(BuildContext context) {
@@ -116,7 +60,7 @@ class _LoginPageState extends State<LoginPage> {
                     child: SizedBox(
                       width: 250,
                       child: ElevatedButton(
-                        onPressed: loginCourier,
+                        onPressed: ( (){ }),
                         style:  ElevatedButton.styleFrom(
                           backgroundColor: const Color.fromRGBO(255, 159, 67,1.0),
                           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)) ,
