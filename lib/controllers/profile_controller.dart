251f8af14516b59d9e7bf924b4632f03512b457d
khalidfahmy98 <khaled741@beone.sa>
2024-08-31 15:07:12 +0300

    created profile page and controller

---
+++ lib/controllers/profile_controller.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:beonecouriers/homepage.dart';
+import 'package:flutter/material.dart';
+import '../Core/apiCore.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ProfileController extends GetxController {
+
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+ 
+    Future<Map<String, dynamic>> fetchProfile() async {
+      final  SharedPreferences? prefs = await _prefs;
+      var token = prefs?.getString('token').toString();
+        var headers = {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+      'Authorization': 'Bearer $token',
+    };
+    var url = Uri.parse(demoBaseUrl + methods[3]);
+    try {
+      print(token);
+      final res = await http.get(url,headers: headers);
+      final data = jsonDecode(res.body);
+      print(res.body);
+      if (data['status'] == 200 ){
+        print (data);
+      }
+      // if (res.statusCode == 200) {
+      //   final SharedPreferences? prefId = await _prefs;
+      //   await prefId?.setString('id', data['data']['id']);
+      // }
+      // print (data);
+      return data;
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+
+
+
+}
--- lib/profilePage.dart
+++ lib/profilePage.dart
@@ -1,3 +1,4 @@
+import 'package:beonecouriers/controllers/profile_controller.dart';
 import 'package:beonecouriers/includes/barCard.dart';
 import 'package:beonecouriers/includes/menuTabs.dart';
 import 'package:beonecouriers/includes/pageLabel.dart';
@@ -14,50 +15,87 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-
-final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  ProfileController profileController = Get.put(ProfileController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          children: [
-            const BarCard(),
-            Expanded(child: Padding(
-              padding:const EdgeInsets.all(12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const PageLabel(subject: 'My Profile '),
-                  //  Text(box.read('authKey').toString()),
-                  Container(
-                    alignment:Alignment.center,
-                    child: SizedBox(
-                      width:250,
-                      child: ElevatedButton(
-                            onPressed: () async {
-                              final SharedPreferences? prefs = await _prefs;
-                              prefs?.clear();
-                              Get.offAll(const LoginPage());
-                            } ,
-                            style:  ElevatedButton.styleFrom(
-                              backgroundColor: const Color.fromRGBO(255, 159, 67,1.0),
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)) ,
-                            ),
-                            child: const Text(
-                              'Logout',
-                              style: TextStyle(
-                                  color: Colors.white, fontWeight: FontWeight.bold),
-                            ),
+      body: Column(
+        children: [
+          const BarCard(),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const PageLabel(subject: 'My Profile '),
+                FutureBuilder(
+                    future: profileController.fetchProfile(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator.adaptive(),
+                        );
+                      }
+                      if (snapshot.hasError) {
+                        return Text(snapshot.error.toString());
+                      }
+                      if (!snapshot.hasData) {
+                        return const Center(
+                          child: Text('No information to list here '),
+                        );
+                      }
+                      final data = snapshot.data!;
+                      final id = data['data']['id'].toString();
+                      final name = data['data']['name'];
+                      final email = data['data']['email'];
+                      return Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              Text('Identifier : $id'),
+                              const SizedBox(height: 10),
+                              Text('Name : $name'),
+                              const SizedBox(height: 10),
+                              Text('Email : $email'),
+                            ],
                           ),
+                        ),
+                      );
+                    }),
+                Container(
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: 250,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final SharedPreferences? prefs = await _prefs;
+                        prefs?.clear();
+                        Get.offAll(const LoginPage());
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromRGBO(255, 159, 67, 1.0),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                      ),
+                      child: const Text(
+                        'Logout',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
                     ),
                   ),
-                ],
-              ),
-            )),
-            const MenuTabs(selectedIndex:  2),
-          ],
-        ),
-    ) ;
+                ),
+              ],
+            ),
+          )),
+          const MenuTabs(selectedIndex: 2),
+        ],
+      ),
+    );
   }
 }
