4bb72897dd8fe83de688c39a9d57374970c444b1
khalidfahmy98 <khaled741@beone.sa>
2024-09-08 16:56:04 +0300

    debug banner hidden

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'BeOne Express Courier',
       theme: ThemeData(
         fontFamily: 'Lato',
